b36b2e51273a0747b413101e47235a2f42f8a3af
DaveJump <983136557@qq.com>
2016-07-02 02:34:13 +0800

    index layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,37 @@
-/**
- * Created by Administrator on 2016/6/30.
- */
+$(function(){
+  $(".banner-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    smartSpeed: 600
+  });
+
+  $(".best-sales-carousel").owlCarousel({
+    items: 3,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    autoplayHoverPause: true,
+    smartSpeed: 600,
+    responsive:{
+      0:{
+        items: 1
+      },
+      769:{
+        items: 2
+      },
+      993:{
+        items: 3
+      }
+    }
+  });
+
+  $('.nav-icon-button').click(function(e){
+    $('.nav-main').slideToggle(300);
+
+    e.stopPropagation();
+    e.preventDefault();
+  });
+});
