e63b25955a250fcb6219156d7518138156e37cb6
DaveJump <983136557@qq.com>
2016-07-04 23:53:17 +0800

    layout finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,4 @@
-$(function(){
-  $(".banner-carousel").owlCarousel({
-    items: 1,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 4000,
-    autoplayHoverPause: true,
-    smartSpeed: 600
-  });
-
-  $(".best-sales-carousel").owlCarousel({
-    items: 3,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 6000,
-    autoplayHoverPause: true,
-    smartSpeed: 600,
-    responsive:{
-      0:{
-        items: 1
-      },
-      769:{
-        items: 2
-      },
-      993:{
-        items: 3
-      }
-    }
-  });
+;(function(){
 
   $('.nav-icon-button').click(function(e){
     $('.nav-main').slideToggle(300);
@@ -34,4 +6,20 @@ $(function(){
     e.stopPropagation();
     e.preventDefault();
   });
-});
+
+  var pic_small = $('.pic-s');
+  var pic_big = $('.pic-l').find('ul');
+
+  pic_small.on('click','li',function(){
+    $(this).siblings('li').removeClass('current');
+    $(this).addClass('current');
+
+    var index = $(this).index();
+
+    pic_big.css({
+      transform: 'translate3d(-'+ (index * 100) / 4 +'%,0,0)',
+      webkitTransform: 'translate3d(-'+ (index * 100) / 4 +'%,0,0)'
+    });
+  });
+
+})();
